d8a4aff5f8b509aaed886ae68829e9814c59af26
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-07-24 14:00:45 +0000

    [In Progress]2022.07.24.23:00

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,37 @@
 class BooksController < ApplicationController
+ 
+  def create
+    book = Book.new(book_params)
+    book.save
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to list_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ　require(:book).
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
